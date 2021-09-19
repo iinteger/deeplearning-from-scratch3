7424cac02068b2e1e59e5f3a04703f729d2f1ba4
leejungsoo <love910321@naver.com>
2021-09-19 16:51:31 +0900

    add step08, 09. optimize backpropagation

---
+++ steps/step09.py
@@ -0,0 +1,87 @@
+import numpy as np
+
+
+def square(x):
+    return Square()(x)
+
+
+def exp(x):
+    return Exp()(x)
+
+
+def as_array(x):
+    if np.isscalar(x):
+        return np.array(x)
+    return x
+
+
+class Variable:
+    def __init__(self, data):
+        if data is not None:
+            if not isinstance(data, np.ndarray):  # 입력받은 데이터가 ndarray가 아니면 에러 발생
+                raise TypeError("{}는 지원하지 않습니다.".format(type(data)))
+
+        self.data = data
+        self.grad = None  # data와 대응하는 미분값
+        self.creator = None
+
+    def set_creator(self, func):  # 변수의 관점에서 함수는 자신을 만들어낸 창조자(creator)
+        self.creator = func
+
+    def backward(self):  # 반복문을 사용해 구현.
+        if self.grad is None:  # 기울기가 없을때 자동으로 초기 미분값 생성
+            self.grad = np.ones_like(self.data)
+
+        funcs = [self.creator]
+        while funcs:
+            f = funcs.pop()
+            x, y = f.input, f.output
+            x.grad = f.backward(y.grad)
+
+            if x.creator is not None:
+                funcs.append(x.creator)
+
+
+class Function:
+    def __call__(self, input):
+        x = input.data
+        y = self.forward(x)
+        output = Variable(as_array(y))  # 계산이 끝난 결과를 클래스에 담아서 return
+        output.set_creator(self)  # 이 함수가 변수의 창조자임을 저장. 자동 역전파에서 이를 참조
+        self.input = input  # 입력 변수 기억. backward에서 재사용
+        self.output = output  # 출력 저장
+        return output
+
+    def forward(self, x):
+        raise NotImplementedError()
+
+    def backward(self, gy):
+        raise NotImplementedError()
+
+
+class Square(Function):
+    def forward(self, x):
+        y = x ** 2
+        return y
+
+    def backward(self, gy):
+        x = self.input.data
+        gx = 2 * x * gy
+        return gx
+
+
+class Exp(Function):
+    def forward(self, x):
+        y = np.exp(x)
+        return y
+
+    def backward(self, gy):
+        x = self.input.data
+        gx = np.exp(x) * gy
+        return gx
+
+
+if __name__ == '__main__':
+    x = Variable(np.array(1.0))
+    x = Variable(None)
+    x = Variable(1.0)
