b0f48caa5c5ca99e1edd1510fa47ea7380aabd75
leejungsoo <love910321@naver.com>
2021-10-28 13:32:34 +0900

    add step33. newton optimization

---
+++ steps/step33.py
@@ -0,0 +1,23 @@
+import numpy as np
+from dezero import Variable
+
+def f(x):
+    y = x ** 4 - 2 * x ** 2
+    return y
+
+x = Variable(np.array(2.0))
+iters = 10
+
+for i in range(iters):
+    print(i, x)
+
+    y = f(x)
+    x.cleargrad()
+    y.backward(create_graph=True)
+
+    gx = x.grad
+    x.cleargrad()
+    gx.backward()
+    gx2 = x.grad
+
+    x.data -= gx.data / gx2.data
