e51ec7b3e64426508ee2bfd4362367cd92b4afdd
JungSoo Lee <love910321@naver.com>
2021-11-12 12:58:48 +0900

    add step 36, 37

---
+++ steps/step37.py
@@ -0,0 +1,15 @@
+import numpy as np
+from dezero import Variable
+import dezero.functions as F
+
+
+x = Variable(np.array([[1, 2, 3], [4, 5, 6]]))
+c = Variable(np.array([[10, 20, 30], [40, 50, 60]]))
+t = x + c
+y = F.sum(t)
+
+y.backward(retain_grad=True)
+print(y.grad)
+print(t.grad)
+print(x.grad)
+print(c.grad)
