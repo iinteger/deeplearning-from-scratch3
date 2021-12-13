4d86892760033c7d969698d40c25879ffbae7624
leejungsoo <love910321@naver.com>
2021-12-13 19:58:13 +0900

    add step41. matrix multiplication

--- dezero/functions.py
+++ dezero/functions.py
@@ -143,5 +143,18 @@ def sum_to(x, shape):
     return SumTo(shape)(x)
 
 
+class MatMul(Function):
+    def forward(self, x, W):
+        y = x.dot(W)
+        return y
 
+    def backward(self, gy):
+        x, W = self.inputs
+        gx = matmul(gy, W.T)
+        gW = matmul(x.T, gy)
+        return gx, gW
+
+
+def matmul(x, W):
+    return MatMul()(x, W)
 
