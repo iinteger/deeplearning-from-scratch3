3abb9591416c12c56c2b38774c18aaa1a356ff7e
leejungsoo <love910321@naver.com>
2022-01-09 21:55:28 +0900

    add step26. optimizers

--- dezero/__init__.py
+++ dezero/__init__.py
@@ -11,13 +11,14 @@ if is_simple_core:
 
 else:
     from dezero.core import Variable
+    from dezero.core import Parameter
     from dezero.core import Function
     from dezero.core import using_config
     from dezero.core import no_grad
     from dezero.core import as_array
     from dezero.core import as_variable
     from dezero.core import setup_variable
-    from dezero.core import Parameter
+    from dezero.core import Config
     from dezero.layers import Layer
     from dezero.models import Model
 
